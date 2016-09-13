df017ec5d13811343329513c33d04da445538ae2
Anthony Sherbondy <ts1235813@gmail.com>
2016-09-13 13:38:34 -0700

    swipe refresh

--- androids/Flixster/app/src/main/java/com/anthonysherbondy/flixster/MovieActivity.java
+++ androids/Flixster/app/src/main/java/com/anthonysherbondy/flixster/MovieActivity.java
@@ -1,6 +1,7 @@
 package com.anthonysherbondy.flixster;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
@@ -23,11 +24,24 @@ public class MovieActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
     ListView lvItems;
     MovieArrayAdapter movieAdapter;
+    SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchMoviesAsync();
+            }
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
         Log.d("DEBUG", "onCreate: called");
 
@@ -36,12 +50,16 @@ public class MovieActivity extends AppCompatActivity {
         movieAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(movieAdapter);
 
-        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        fetchMoviesAsync();
+    }
 
+    private void fetchMoviesAsync() {
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                swipeContainer.setRefreshing(false);
                 JSONArray movieJsonResults = null;
                 try {
                     movieJsonResults = response.getJSONArray("results");
@@ -55,6 +73,7 @@ public class MovieActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                swipeContainer.setRefreshing(false);
                 super.onFailure(statusCode, headers, responseString, throwable);
             }
         });
