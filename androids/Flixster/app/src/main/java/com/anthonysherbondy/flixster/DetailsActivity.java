2ade92872b4b9c8c219a66f3dd7b0d36ee94b48d
Anthony Sherbondy <ts1235813@gmail.com>
2016-09-14 12:42:42 -0700

    scrollview for details

--- androids/Flixster/app/src/main/java/com/anthonysherbondy/flixster/DetailsActivity.java
+++ androids/Flixster/app/src/main/java/com/anthonysherbondy/flixster/DetailsActivity.java
@@ -26,16 +26,13 @@ public class DetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvSynopsis = (TextView) findViewById(R.id.tvSynopsis);
         rbVotes = (RatingBar) findViewById(R.id.rbVote);
+        ivMovieImage = (ImageView) findViewById(R.id.ivMovieImage);
 
         Movie movie = (Movie) getIntent().getSerializableExtra("movie");
         tvTitle.setText(movie.getOriginalTitle());
         tvSynopsis.setText(movie.getOverview());
-
-//        int numStars = (int) Math.round(movie.getVoteAverage());
-//        Log.d("DEBUG", String.format("stars %d", numStars));
         rbVotes.setRating((float) movie.getVoteAverage());
 
-        ivMovieImage = (ImageView) findViewById(R.id.ivMovieImage);
         Picasso.with(this).load(movie.getBackdropPath())
                 .transform(new RoundedCornersTransformation(10, 3))
                 .placeholder(R.drawable.ic_camera_black_24dp)
