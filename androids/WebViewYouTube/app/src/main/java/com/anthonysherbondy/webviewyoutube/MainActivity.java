8c082261b024bec40325c2e044795efc860c6b6c
Anthony Sherbondy <ts1235813@gmail.com>
2016-10-30 20:18:32 -0700

    Added WebView playing you tube example, or non-example as it fails

---
+++ androids/WebViewYouTube/app/src/main/java/com/anthonysherbondy/webviewyoutube/MainActivity.java
@@ -0,0 +1,55 @@
+package com.anthonysherbondy.webviewyoutube;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        String frameVideo = "<html><body>Youtube video .. <br> <iframe width=\"320\" height=\"315\" src=\"https://www.youtube.com/embed/lY2H2ZP56K4\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+        myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.setWebChromeClient(new WebChromeClient() {
+
+        });
+        // Configure related browser settings
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+        myWebView.setWebViewClient(new MyBrowser());
+
+
+        // Load the initial URL
+        myWebView.loadUrl("https://www.youtube.com/watch?v=aoCP_JHzBUM");
+//        myWebView.loadData(frameVideo, "text/html", "utf-8");
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+    }
+}
